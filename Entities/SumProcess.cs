1bf7d39848aaed8b6037c580858e5ba0305b09f2
Gustavo Basso <gustavo.basso01@hotmail.com>
2023-10-05 22:40:34 -0300

    general commit

---
+++ Entities/SumProcess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesTestes.Entities {
+    internal class SumProcess {
+
+        public string Item { get; set; }
+        public double Price { get; set; }
+        public int Quant { get; set; }
+        public double TotalPrice { get; set; }
+
+        public SumProcess() { }
+        public SumProcess(string item, double price, int quant) {
+
+            Item = item;
+            Price = price;
+            Quant = quant;
+
+        }
+
+        public void totalPrice() {
+
+            TotalPrice = Quant * Price;
+        }
+
+        public override string ToString() {
+
+            return Item + " (" + TotalPrice + ")";
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using FilesTestes.Entities;
+
+class Program {
+
+
+    static void Main(string[] args) {
+
+
+        string path = @"c:\AMD\Aula\Atv.csv";
+        string pathCreateDirectory = @"c:\AMD\Aula\Out";
+        string pathCreateFile = @"c:\AMD\Aula\Out\Sumary.csv";
+        DirectoryInfo df = new DirectoryInfo(path);
+        StreamReader sr;
+        List<SumProcess> sm = new List<SumProcess>();
+        try {
+
+            Directory.CreateDirectory(pathCreateDirectory);
+
+            string name;
+            int quanty;
+            double price;
+
+            using (sr = new StreamReader(path)) { 
+            
+                while( !sr.EndOfStream ) {
+
+                    string[] line = sr.ReadLine().Split(",");
+                    name = line[0];
+                    quanty = int.Parse(line[1]);
+                    price = double.Parse(line[2]);
+                    
+                    sm.Add(new SumProcess(name, price, quanty));
+                }
+            }
+            foreach(SumProcess line in sm) {
+
+                Console.WriteLine(line.Item + " - " + line.Quant + " - " + line.Price);
+
+            }
+            File.Create(pathCreateFile).Close();
+            using (StreamWriter sw = new StreamWriter(pathCreateFile)){ 
+            
+                foreach(SumProcess line in sm) {
+
+                    line.totalPrice();
+                    sw.WriteLine(line.ToString());
+                }
+            
+            }
+
+        }
+        catch (IOException ex) {
+
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+
+
+    }
+}
